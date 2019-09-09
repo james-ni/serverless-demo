d97d256edaaf308874d501f5c94669f17ef226c9
guangtoutou <ni.ningning@gmail.com>
2019-09-10 00:26:02 +1000

    created an API subscribed to SNS

---
+++ handler.py
@@ -0,0 +1,30 @@
+import json
+
+
+def hello(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
+
+def sub_sns(event, context):
+    # print("Received event: " + json.dumps(event, indent=2))
+    message = event['Records'][0]['Sns']['Message']
+    print("From SNS: " + message)
+    return message
